47d96f6d4edd7fd19423e89180b8e14ccb5ad917
Pvmpkin <baguness822@gmail.com>
2026-04-16 10:00:25 +1000

    Working progress_Star Quiz trial one
    Signed-off-by: Pvmpkin <baguness822@gmail.com>

--- src/MainCLI.java
+++ src/MainCLI.java
@@ -4,6 +4,10 @@ public class MainCLI{
     public static void main(String[] args)throws Exception{
         StrInterfaces strInterfaces = new StrInterfaces();
         strInterfaces.Menu();
+        strInterfaces.HistoryMenu();
+        strInterfaces.QuizMenu();
+        strInterfaces.SelectTopic();
+        strInterfaces.SettingsMenu();
 
     }
 
